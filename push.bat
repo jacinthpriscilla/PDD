2651290b87b378c92f74ec25559e4d7babc08132
jacinthpriscilla <jacinthpriscilla8@gmail.com>
2026-08-06 13:42:31 +0530

    add helper push script

---
+++ push.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ==========================================
+echo Pushing PDD repository to GitHub...
+echo ==========================================
+git push -u origin main
+echo ==========================================
+echo Finished! Press any key to close.
+echo ==========================================
+pause
